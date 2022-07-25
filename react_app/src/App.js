d357158602011024c244bf510e14111652e7e873
anurart kaewlamul <ikungcomen@gmail.com>
2022-07-25 22:22:27 +0700

    login form

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -26,8 +26,9 @@ function App() {
 */
 
 
-
+/*
 class App extends React.Component{
+  
   constructor() {
     super();
       this.state = {
@@ -56,8 +57,13 @@ class App extends React.Component{
           </div>
       );
     }
-}
+    
 
+
+    
+}*/
+
+/*
 class StudentName extends React.Component {
      render() {
       return(
@@ -77,5 +83,53 @@ class StudentName extends React.Component {
         );
       }
    }
-  
+
+*/
+function App(){
+  return (
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col-12 col-md-6 offset-md-3">
+          <h2 className="my-4 text-center">LOGIN</h2>
+
+          <form>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <div className="form-check mb-4">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" for="exampleCheck1">
+                Accept term and conditions
+              </label>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default App;
